b301e3ef5ef75f2c4158f0a7704c78bc877b0eb6
Jackw9829 <junming883@gmail.com>
2026-06-11 11:03:45 +0800

    remove all files

--- CyberLearnHub/Chatbot.aspx.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public partial class Chatbot : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        // Page loads normally — chatbot runs via JavaScript + ChatbotHandler.ashx
-    }
-}
